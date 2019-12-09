bbcba95bf76704dc22f12b5d315fdfb56590f7a8
fy <fy@znjf33.com>
2019-12-09 18:10:32 +0800

    修改

--- file/sort.js
+++ file/sort.js
@@ -1,5 +1,6 @@
 function Sort() {
   Sort.prototype.bubbles =function(arry){
+    //从小到大
     var temp;
     for (var i = 0; i < arry.length-1; i++) {
         for (var j = 0; j < arry.length-1-i;j++) {
@@ -11,6 +12,22 @@ function Sort() {
       }
     }
       return arry
+  },
+  Sort.prototype.selector = function(arr){
+    var len = arr.length;
+     var minIndex, temp;
+     for (var i = 0; i < len - 1; i++) {
+         minIndex = i;
+         for (var j = i + 1; j < len; j++) {
+             if (arr[j] < arr[minIndex]) {     // 寻找最小的数
+                 minIndex = j;                 // 将最小数的索引保存
+             }
+         }
+         temp = arr[i];
+         arr[i] = arr[minIndex];
+         arr[minIndex] = temp;
+     }
+     return arr;
   }
 
 }
