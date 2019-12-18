59b7e6395399c132ff9c5393724f5aea3d212742
fy <fy@znjf33.com>
2019-12-18 08:49:08 +0800

    快排

--- file/sort.js
+++ file/sort.js
@@ -45,4 +45,23 @@ function Sort() {
     return arr;
   }
 
+  Sort.prototype.quickSort = function(array){
+    if (array.length <= 1) {
+        return array;
+    }
+    const pivotIndex = Math.floor(array.length / 2);
+    const pivot = array.splice(pivotIndex, 1)[0];  //从数组中取出我们的"基准"元素
+    console.log(array);
+    const left = [], right = [];
+    array.forEach(item => {
+        if (item < pivot) {  //left 存放比 pivot 小的元素
+            left.push(item);
+        } else {  //right 存放大于或等于 pivot 的元素
+            right.push(item);
+        }
+    });
+    //至此，我们将数组分成了left和right两个部分
+    return this.quickSort(left).concat(pivot, this.quickSort(right));
+  }
+
 }
